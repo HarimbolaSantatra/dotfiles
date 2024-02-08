a70b13fac5f5d45d7f00f74d460f371943abd756
santatra <razafindratsimba34@gmail.com>
2024-02-08 21:02:32 +0300

    edit python install script and add utils install script

--- install-script/python-install.sh
+++ install-script/python-install.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
+installed_ver=$(python --version)
+used_ver="3.10"
+echo "Set python version variable."
+echo "Current one is: $ver"
 
 sudo apt-get update
+
 # Install python debugger
 sudo apt install python-dev-is-python3
+
+# Install python virtual environment (venv)
+sudo apt install -y python${used_ver}-venv
---
+++ install-script/utils.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+echo "Installing figlet ..."
+sudo apt-get install -y figlet
