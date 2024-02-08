720b2024934c2d40528dac06dcc796c687bf696e
santatra <razafindratsimba34@gmail.com>
2024-02-08 09:39:30 +0300

    Add bash shebang

--- install-script/yarn-install.sh
+++ install-script/yarn-install.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 # Installation tutorial: https://classic.yarnpkg.com/lang/en/docs/install/#debian-stable
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
