4ead9091fd50a27ddce22539f57341e6506ff094
santatra <razafindratsimba34@gmail.com>
2024-02-10 08:20:32 +0300

    add thefuck

--- install-script/utils.sh
+++ install-script/utils.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 echo "Installing figlet ..."
 sudo apt-get install -y figlet
+
+echo "Installing thefuck ..."
+sudo apt install python3-dev python3-pip python3-setuptools
+pip3 install thefuck --user
