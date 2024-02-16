8e6473a1bf51ade7b31b9c534c90653fa0421008
santatra <razafindratsimba34@gmail.com>
2024-02-16 15:03:42 +0300

    add hacking and network installaton script

---
+++ install-script/hacking.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+set -e
+sudo apt update
+# John the ripper
+sudo apt-get install john -y
---
+++ install-script/network.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -e
+sudo apt-get update
+# Nmap
+sudo apt-get install -y nmap
+# netcat | nc
+sudo apt-get install -y netcat
