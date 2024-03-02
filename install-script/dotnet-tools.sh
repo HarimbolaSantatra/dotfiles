05960cedb45d40eb3d410274a18468ee21dfcae4
santatra <razafindratsimba34@gmail.com>
2024-03-02 20:55:46 +0300

    install: add dotnet ef

---
+++ install-script/dotnet-tools.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+# https://learn.microsoft.com/en-us/ef/core/cli/dotnet#installing-the-tools
+dotnet tool install --global dotnet-ef
+dotnet add package Microsoft.EntityFrameworkCore.Design
