87868455d1032c84d64b9ba605f87bc896719f67
santatra <razafindratsimba34@gmail.com>
2024-03-02 12:03:28 +0300

    install: rename jenkins

--- install-script/jenkins.sh
+++ install-script/jenkins.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
       https://pkg.jenkins.io/debian/jenkins.io-2023.key
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
