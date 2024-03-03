e59f310d81b09bde26df532304f2e2769a12bbc7
santatra <razafindratsimba34@gmail.com>
2024-03-03 14:56:29 +0300

    setup: edit flags

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
-rsync_flags="-auv"
+# http://google.com
+rsync_flags="-auP"
 config_path="/home/$USER/.config"
 rsync $rsync_flags fish/ $config_path/fish/
 rsync $rsync_flags i3/config $config_path/i3/
 rsync $rsync_flags nvim/ $config_path/nvim/
 rsync $rsync_flags .bashrc .bash_aliases $HOME
+rsync $rsync_flags .tmux.conf $HOME
