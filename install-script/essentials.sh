88b8fb3d74aa026c55451ea56a139f1d52c14d91
santatra <razafindratsimba34@gmail.com>
2024-03-10 15:09:16 +0300

    install: add essentials.sh

---
+++ install-script/essentials.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# auto-tools
+sudo apt-get install -y autotools-dev
+sudo apt-get install -y automake
+
+# Git
+sudo apt-get install -y git
+
+# GnuTLS: tools to generate certificates for the verification of host identity and the encryption of client/server communications. 
+# in order to clone a github repo
+sudo apt install -y gnutls-bin
+
+# install tmux
+sudo apt-get install -y tmux
