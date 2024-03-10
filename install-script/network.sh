79c4eaf0e908873525759253d431db638d079ab4
santatra <razafindratsimba34@gmail.com>
2024-03-10 15:09:48 +0300

    install: add net-tools

--- install-script/network.sh
+++ install-script/network.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -e
 sudo apt-get update
+# net-tools: arp, ifconfig, route, netstat
+sudo apt install net-tools
 # Nmap
 sudo apt-get install -y nmap
 # netcat and wireshark
