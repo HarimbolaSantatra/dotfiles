848ffd1842a4ef9143672949d275b4b24a7d0806
santatra <razafindratsimba34@gmail.com>
2024-03-20 14:03:18 +0300

    edit bookmarks binary

--- bin/fix-bookmarks.sh
+++ bin/fix-bookmarks.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-bookmarks_file="./bookmarks.html"
-if test ! -e $bookmarks_file; then
+set -e
+bookmarks_file="$(pwd)/bookmarks.html"
+if test ! -e "$bookmarks_file"; then
     echo "$bookmarks_file doesn't exist!"
     echo "Import it first from Firefox."
     exit 1
@@ -11,4 +12,5 @@ quickmarks="$config/qutebrowser/quickmarks"
 # Clear all quickmarks
 truncate -s 0 "$quickmarks"
 
-python /usr/share/qutebrowser/scripts/importer.py $bookmarks_file >> "$quickmarks"
+python /usr/share/qutebrowser/scripts/importer.py "$bookmarks_file" >> "$quickmarks"
+echo "Bookmarks imported!"
