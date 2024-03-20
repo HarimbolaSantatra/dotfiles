38d7910a6c2be7bce6db990b1dd05c84dc98840a
santatra <razafindratsimba34@gmail.com>
2024-03-20 20:12:18 +0300

    bookmarks: add qutebrowser's quickmarks

--- bin/fix-bookmarks.sh
+++ bin/fix-bookmarks.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 set -e
-bookmarks_file="$(pwd)/bookmarks.html"
+bookmarks_file="$(pwd)/bookmarks/bookmarks.html"
 if test ! -e "$bookmarks_file"; then
     echo "$bookmarks_file doesn't exist!"
     echo "Import it first from Firefox."
     exit 1
 fi
 
+quickmarks_file="$(pwd)/bookmarks/quickmarks.html"
+
 config="$HOME/.config"
 quickmarks="$config/qutebrowser/quickmarks"
-# Clear all quickmarks
-truncate -s 0 "$quickmarks"
 
-python /usr/share/qutebrowser/scripts/importer.py "$bookmarks_file" >> "$quickmarks"
+# Import quickmarks
+rsync -uav $quickmarks_file $quickmarks
+
+# Uncomment this if you want to import bookmarks in quickmarks
+# truncate -s 0 "$quickmarks" # Clear quickmarks
+#python /usr/share/qutebrowser/scripts/importer.py "$bookmarks_file" >> "$quickmarks"
 echo "Bookmarks imported!"
